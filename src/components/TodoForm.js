6b1ac25373109aa46839230d680890e7e749951d
TaVon <tsyuille@gmail.com>
2022-07-31 17:58:53 -0400

    Add comments to code

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
+    // value of the state and funtion that updates it
     const [input, setInput] = useState(props.edit ? props.edit.value : '')
 
     const inputFocus = useRef(null)
@@ -9,20 +10,25 @@ function TodoForm(props) {
         inputFocus.current.focus()
     })
 
+    // function to set input to what user types in
     const handleChange = e => {
         setInput(e.target.value)
     }
 
+    // function for submitting a new todo
     const handleSubmit = e => {
         e.preventDefault()
         props.onSubmit({
+            // creates an id for the todo list item
             id: Math.floor(Math.random() * 10000), 
             text: input
         })
+        // resets input back to empty string after todo item has been submitted
         setInput('')
     }
 
   return (
+    // form for adding/updating todo list items
     <form className= 'todoForm' onSubmit={handleSubmit}>
         {props.edit ? ( 
         <>
